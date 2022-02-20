226156caf9e14aa41305d3bbce36f9a63731d1c6
Logan <fr0z3n568@gmail.com>
2022-02-20 09:18:23 -0500

    resolves #10

--- src/main/java/com/la3ypotato/toolrenter/console/Console.java
+++ src/main/java/com/la3ypotato/toolrenter/console/Console.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 
 public class Console {
     // UI Messages
-    private String WELCOME_MSG = "Welcome to...";
-    private String EXIT_MSG = "Exiting Tool Renter...";
-    private String INVALID_INPUT_MSG = "Invalid input detected. Please enter a valid option.";
-    private String MENU_GUIDANCE_MSG = "Key in an option below to select it, then press Enter.";
-    private String OPTION_1_TEXT = "1) Checkout a Tool";
-    private String OPTION_2_TEXT = "2) Exit";
+    private final String WELCOME_MSG = "Welcome to...";
+    private final String EXIT_MSG = "Exiting Tool Renter...";
+    // Menu IU Messages
+    private final String INVALID_INPUT_MSG = "Invalid input detected. Please enter a valid option.";
+    private final String MENU_GUIDANCE_MSG = "Key in an option below to select it, then press Enter.";
+    private final String MENU_OPTION_1_TEXT = "1) Checkout a Tool";
+    private final String MENU_OPTION_2_TEXT = "2) Exit";
+    // Checkout UI Messages
+    private final String CHECKOUT_MENU_GUIDANCE_MSG = "Please key in the following information and press enter after" +
+                                                      " each prompt.";
+    private final String CHECKOUT_PROMPT_1 = "Enter the tool code for the rental tool";
+    private final String CHECKOUT_PROMPT_2 = "Enter the number of rental days.";
+    private final String CHECKOUT_PROMPT_3 = "Enter the discount percent";
+    private final String CHECKOUT_PROMPT_4 = "Enter the checkout date (using mm/dd/yyyy format)";
     // UI OPTIONS
-    private final String OPTION_1 = "1";
-    private final String OPTION_2 = "2";
+    private final String MENU_OPTION_1 = "1";
+    private final String MENU_OPTION_2 = "2";
+    private final int CHECKOUT_OPTION_1 = 1;
+    private final int CHECKOUT_OPTION_2 = 2;
+    private final int CHECKOUT_OPTION_3 = 3;
+    private final int CHECKOUT_OPTION_4 = 4;
     // Class properties
     private boolean consoleExited = true;
     private String errorMsg = "";
@@ -24,12 +36,22 @@ public class Console {
     }
 
     // Shows the tool renter logo
-    private void printHeader() {
+    private void printMenuHeader() {
+        System.out.println(WELCOME_MSG);
         System.out.println(" ______          _____           __        ");
         System.out.println("/_  __/__  ___  / / _ \\___ ___  / /____ ____");
         System.out.println(" / / / _ \\/ _ \\/ / , _/ -_) _ \\/ __/ -_) __/");
-        System.out.println("/_/  \\___/\\___/_/_/|_|\\__/_//_/\\__/\\__/_/   ");
-        System.out.println();
+        System.out.println("/_/  \\___/\\___/_/_/|_|\\__/_//_/\\__/\\__/_/   \n");
+    }
+
+    private void printCheckoutMenuHeader() {
+        System.out.println(" ______          _____           __          ");
+        System.out.println("/_  __/__  ___  / / _ \\___ ___  / /____ ____   ");
+        System.out.println(" / / / _ \\/ _ \\/ / , _/ -_) _ \\/ __/ -_) __/   ");
+        System.out.println("/_/__\\___/\\___/_/_/|_|\\__/_//_/\\__/\\__/_/      ");
+        System.out.println(" / ___/ /  ___ ____/ /_____  __ __/ /_         ");
+        System.out.println("/ /__/ _ \\/ -_) __/  '_/ _ \\/ // / __/         ");
+        System.out.println("\\___/_//_/\\__/\\__/_/\\_\\\\___/\\_,_/\\__/          \n");
     }
 
     // Clears the command line
@@ -47,44 +69,88 @@ public class Console {
     }
 
     // Shows the main menu of tool renter.
-    private void showMenu() {
-        printHeader();
+    private void showMainMenu() {
+        printMenuHeader();
         if (errorMsg != "") {
             System.out.println("!! " + errorMsg + " !!\n");
             errorMsg = "";
         }
         // Displaying menu options and guidance.
         System.out.println(MENU_GUIDANCE_MSG);
-        System.out.println(OPTION_1_TEXT);
-        System.out.println(OPTION_2_TEXT);
+        System.out.println(MENU_OPTION_1_TEXT);
+        System.out.println(MENU_OPTION_2_TEXT);
+    }
+
+    private void showCheckoutMenu() {
+        printCheckoutMenuHeader();
+        System.out.println(CHECKOUT_MENU_GUIDANCE_MSG);
+        // TODO: Load tools
     }
 
     // When called, requests user input.
-    private void requestUserInput() {
+    private void startMainMenu() {
         Scanner scanner = new Scanner(System.in);
         try {
             while (!isConsoleExited()) {
                 clearConsole();
-                showMenu();
+                showMainMenu();
                 String line = scanner.nextLine();
-                handleInput(line);
+                handleMenuInput(line);
             }
         } catch (IllegalStateException | NoSuchElementException e) {
             exitApp();
         }
     }
 
+    private void startCheckoutMenu() {
+        Scanner scanner = new Scanner(System.in);
+        clearConsole();
+        showCheckoutMenu();
+        try {
+            // Displaying prompts and requesting for input.
+            System.out.println(CHECKOUT_PROMPT_1);
+            String line = scanner.nextLine();
+            System.out.println("\n" + CHECKOUT_PROMPT_2);
+            line = scanner.nextLine();
+            System.out.println("\n" + CHECKOUT_PROMPT_3);
+            line = scanner.nextLine();
+            System.out.println("\n" + CHECKOUT_PROMPT_4);
+            line = scanner.nextLine();
+        } catch (IllegalStateException | NoSuchElementException e) {
+            exitApp();
+        }
+
+    }
+
     // Validates the user input and handles it based on the passed option.
-    private void handleInput(String userInput) {
+    private void handleMenuInput(String userInput) {
         switch (userInput) {
-            case OPTION_1:
+            case MENU_OPTION_1:
+                // Received input to check out a tool.
+                startCheckoutMenu();
                 break;
-            case OPTION_2:
+            case MENU_OPTION_2:
                 // Received input to exit.
                 exitApp();
                 break;
             default:
                 errorMsg = INVALID_INPUT_MSG;
+        }
+    }
+
+    private void handleCheckoutMenuInput(int option, String userInput) {
+        switch(option) {
+            case CHECKOUT_OPTION_1:
+                // TODO: Validate input to see if the tools are present
+                break;
+            case CHECKOUT_OPTION_2:
+                break;
+            case CHECKOUT_OPTION_3:
+                break;
+            case CHECKOUT_OPTION_4:
+                break;
+            default:
+                break;
         }
     }
 
@@ -102,7 +168,6 @@ public class Console {
     // Starts the app loop.
     public void startConsole() {
         clearConsole();
-        System.out.println(WELCOME_MSG);
-        requestUserInput();
+        startMainMenu();
     }
 }
